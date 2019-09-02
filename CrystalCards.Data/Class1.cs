892536710a01c0ed8739522ce6ab0b87c140cc67
jay katie martin <jaykatiemartin@outlook.com>
2019-09-02 18:37:01 +0100

    Adding services , data

---
+++ CrystalCards.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CrystalCards.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ CrystalCards.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CrystalCards.Services
+{
+    public class Class1
+    {
+    }
+}
