97baec728048a295f239d7d640c1ce494bbe9071
jay katie martin <ghosttk@protonmail.com>
2019-10-01 11:35:07 +0100

    adding Order to DB

---
+++ CrystalCards.Data/Migrations/20191001103435_addingOrder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrystalCards.Data.Migrations
+{
+    public partial class addingOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Cards",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Cards");
+        }
+    }
+}
