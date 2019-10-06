132b33502f420d6390455bac0c599773a4c138ba
jay katie martin <ghosttk@protonmail.com>
2019-10-06 20:39:52 +0100

    ..

---
+++ CrystalCards.Data/Migrations/20191006192505_addingActionPoints.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrystalCards.Data.Migrations
+{
+    public partial class addingActionPoints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActionPoint",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: true),
+                    CardId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActionPoint", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ActionPoint_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActionPoint_CardId",
+                table: "ActionPoint",
+                column: "CardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActionPoint");
+        }
+    }
+}
