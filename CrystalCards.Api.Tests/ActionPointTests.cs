6869e01b26388b9843473757900543257b0b0cf5
jay katie martin <ghosttk@protonmail.com>
2019-10-06 14:10:29 +0100

    adding a file for actionPoint tests

---
+++ CrystalCards.Api.Tests/ActionPointTests.cs
@@ -0,0 +1,10 @@
+﻿using CrystalCards.api;
+using Xunit;
+
+namespace CrystalCards.Api.Tests
+{
+    public class ActionPointTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private CustomWebApplicationFactory<Startup> _factory = new CustomWebApplicationFactory<Startup>();
+    }
+}
