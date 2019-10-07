4c56e94093d4fd042044384acf8e1df01a611e70
jay katie martin <jaykatiemartin@outlook.com>
2019-10-07 12:34:32 +0100

    adding moodwall functionality part 1

---
+++ CrystalCards.api/Controllers/MoodWallController.cs
@@ -0,0 +1,20 @@
+﻿using CrystalCards.Data;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrystalCards.Api.Controllers
+{
+    [EnableCors("CorsPolicy")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoodWallController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoodWallController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+}
