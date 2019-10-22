63987dae9a95e61aa7b351ac54b3eb80005ff6ab
kat@katiekatcoder.co.uk <kat@katiekatcoder.co.uk>
2019-10-22 15:38:31 +0100

    print idea card part 2

--- CrystalCards-Web/src/app/cards/card-print/card-print.component.ts
+++ CrystalCards-Web/src/app/cards/card-print/card-print.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Inject, Output } from '@angular/core';
 import { NgxPrintModule } from 'ngx-print'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { OpenCardComponent } from '../open-card/open-card.component';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { ConfigService } from 'src/app/services/config.service';
 
 @Component({
   selector: 'app-card-print',
@@ -10,7 +13,12 @@ import { OpenCardComponent } from '../open-card/open-card.component';
 })
 export class CardPrintComponent implements OnInit {
 
-  constructor( public dialogRef: MatDialogRef<OpenCardComponent>,
+
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService,
+    private configService:ConfigService,
+    public dialogRef: MatDialogRef<OpenCardComponent>,
                @Inject(MAT_DIALOG_DATA) public data: any) { }
 
                Description = this.data.description;
@@ -22,10 +30,17 @@ export class CardPrintComponent implements OnInit {
                ActionPoints = this.data.actionPoints;
                Order = this.data.order;
                Links = this.data.links;
+               images: any = null;
              @Output() id=this.Id;
 
   ngOnInit() {
     this.splitPointsToLists(this.Points);
+    this.getImageURLs();
+  }
+  getImageURLs() {
+      this.apiService.GetImageURLs(this.authService.decodedToken.unique_name,this.Id).subscribe(res => {
+      this.images = res.map(x=> `${this.configService.Images}/${this.authService.decodedToken.unique_name}/${this.id}/${x}`);
+    });
   }
 
   splitPointsToLists(points) {
