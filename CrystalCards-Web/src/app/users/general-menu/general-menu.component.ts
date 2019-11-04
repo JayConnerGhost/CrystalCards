e3fa048a98d25bf736c606702558b0aa7913010f
kat@katiekatcoder.co.uk <kat@katiekatcoder.co.uk>
2019-11-04 16:49:03 +0000

    Cleaning out auto generated test spec to make way for manually created ones once i understand testing better

---
+++ CrystalCards-Web/src/app/users/general-menu/general-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-general-menu',
+  templateUrl: './general-menu.component.html',
+  styleUrls: ['./general-menu.component.css']
+})
+export class GeneralMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
