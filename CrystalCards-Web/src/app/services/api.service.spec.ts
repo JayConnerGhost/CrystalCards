01b83f3587857f400c05de151ced6e4cb261e9e4
kat@katiekatcoder.co.uk <kat@katiekatcoder.co.uk>
2019-11-04 17:03:39 +0000

    simple passing test on the apiservice -starting point

---
+++ CrystalCards-Web/src/app/services/api.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ApiService } from './api.service';
+
+describe('ApiService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
+
+  it('should be created', () => {
+    const service: ApiService = TestBed.get(ApiService);
+    expect(service).toBeTruthy();
+  });
+});
