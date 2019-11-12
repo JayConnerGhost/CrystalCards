49dc4d99a707951798c9a5f63b9ff31fc706be0d
kat@katiekatcoder.co.uk <kat@katiekatcoder.co.uk>
2019-11-12 03:47:35 +0000

    fixing a bug in closing add project

--- CrystalCards-Web/src/app/projects/add-project/add-project.component.ts
+++ CrystalCards-Web/src/app/projects/add-project/add-project.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProjectApiService} from "../../services/project-api.service";
 import {NgForm} from "@angular/forms";
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-add-project',
@@ -9,7 +10,10 @@ import {NgForm} from "@angular/forms";
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor(private projectApiService: ProjectApiService) { }
+  constructor(
+    private projectApiService: ProjectApiService,
+    public dialogRef: MatDialogRef<AddProjectComponent>
+  ) { }
   Id=0;
   Title=null;
   ngOnInit() {
@@ -17,5 +21,6 @@ export class AddProjectComponent implements OnInit {
 
   onSubmit(f: NgForm) {
     this.projectApiService.AddProject(this.Id,this.Title).subscribe();
+    this.dialogRef.close();
   }
 }
