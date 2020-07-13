04d0036d6570693f41479b0577d9bbcb422dd632
Katie Kat Martin <ghosttk@protonmail.com>
2020-07-13 07:42:15 +0100

    Adding basic cardwall service [display]

--- CrystalCards-Web2/src/app/card-token/card-token.component.ts
+++ CrystalCards-Web2/src/app/card-token/card-token.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card-token',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-token.component.css']
 })
 export class CardTokenComponent implements OnInit {
+  @Input() Title: string;
+  @Input() Description: string;
+  @Input() Id : number;
 
   constructor() { }
 
